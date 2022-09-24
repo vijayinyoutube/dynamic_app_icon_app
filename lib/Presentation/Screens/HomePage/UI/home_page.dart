1e3b28e399f2a21dab89c6a877942e20c88a25f5
vijayinyoutube <vijaythevirtue@gmail.com>
2022-09-24 18:20:59 +0530

    refactor

--- lib/Presentation/Screens/HomePage/UI/home_page.dart
+++ lib/Presentation/Screens/HomePage/UI/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:dynamic_app_icon_app/Presentation/Declarations/constants.dart';
+
 import '../../../Components/app_bar.dart';
 import 'package:flutter/material.dart';
+
+import '../../../Components/primary_btn.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -10,6 +14,15 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(appBarTitle: title),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: EdgeInsets.only(bottom: kSpacing*2),
+          child: Align(
+              alignment: Alignment.bottomCenter,
+              child: PrimaryBtn(btnFun: () {}, btnText: 'Change app icon')),
+        ),
+      ),
     );
   }
 }
